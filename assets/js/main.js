b77d5f3f3a5da1a2d63c1c4d97d1ad2f983dff42
Richard Hoffman <coverslide@gmail.com>
2015-01-08 05:34:07 -0800

    Improvements

--- assets/js/main.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-//window.jQuery = window.$ = require('jquery');
-
-var CbzReaderPage = require('./cbz-reader/page/main-page.js');
-
-var page = new CbzReaderPage();
---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+'use strict';
+
+var CbzReaderPage = require('./page/main-page.js');
+var config = require('../../config.json');
+
+var page = new CbzReaderPage(config);
