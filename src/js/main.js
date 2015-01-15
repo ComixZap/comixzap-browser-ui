2363793f9ecbc3b5d4c1cebe9a8cfc8e0510a2a9
Richard Hoffman <coverslide@gmail.com>
2015-01-15 06:32:56 -0800

    Streamline configuration

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
 var CbzReaderPage = require('./page/main-page.js');
-var config = require('../../config.json');
+var config = require('config');
 
 var page = new CbzReaderPage(config);
